ec99bd7cdc7b8951aefdef9c9416369e1406ac5a
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-07-06 13:53:21 +0000

    bulb image done

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ import AnimatedText from "@/components/AnimatedText";
 import Link from "next/link";
 import { LinkArrow } from "@/components/Icons";
 import HireMe from "@/components/HireMe";
+import lightBulb from "../../public/images/svgs/miscellaneous_icons_1.svg"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -59,6 +60,9 @@ export default function Home() {
           </div>
         </Layout>
         <HireMe/>
+        <div className="absolute right-8 bottom-8 inline-block w-24">
+          <Image src={lightBulb} alt="Manasseh" className="w-full h-auto"/>
+        </div>
       </main>
     </>
   );
