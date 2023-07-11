57cae8feea280267d71fe7d010eaac4d1edb8717
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-07-11 10:55:11 +0000

    skills component completed

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,7 @@ import Image from "next/image";
 import React, { useEffect, useRef } from "react";
 import profilePic from "../../public/images/profile/sub2.png";
 import { useInView, useMotionValue, useSpring } from "framer-motion";
+import Skills from "@/components/Skills";
 
 const AnimatedNumbers = ({ value }) => {
   const ref = useRef(null);
@@ -96,6 +97,7 @@ const about = () => {
               </div>
             </div>
           </div>
+          <Skills/>
         </Layout>
       </main>
     </>
